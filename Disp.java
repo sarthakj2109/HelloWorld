838c4835b4816cf0e5ff83904fceb963d9052b0b
sarthakj2109 <54064348+sarthakj2109@users.noreply.github.com>
2022-03-14 20:44:53 +0530

    Added new files

---
+++ Disp.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Disp {
+	public static void main(String [] args)throws IOException{
+		for(int i =0;i<5;i++) {
+			System.out.println("Hello World.. "+i);
+		}
+	}
+
+}
